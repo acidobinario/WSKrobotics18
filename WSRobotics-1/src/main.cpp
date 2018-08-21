337bf480a36194290bd35a2f72f554493589ae54
4C1D0B1N4R10 <fede.vmusica@gmail.com>
2018-08-20 22:04:38 -0300

    first stuff done (functions uwu)

--- WSRobotics-1/src/main.cpp
+++ WSRobotics-1/src/main.cpp
@@ -1,9 +1,54 @@
 #include <Arduino.h>
+#include <PRIZM.h>     // include PRIZM library
+PRIZM prizm;           // instantiate a PRIZM object “prizm” so we can use its functions
 
-void setup() {
-    // put your setup code here, to run once:
+
+//-------easy actions----------
+//-------ARM-----------
+void OpenClaw(){
+  prizm.setServoPosition(2,170);
+}
+
+void CloseClaw(){
+  prizm.setServoPosition(2,10);
+}
+
+void ArmUp(){
+  prizm.setServoPosition(1, 10);
+}
+
+void ArmDown(){
+  prizm.setServoPosition(1, 170);
+}
+//-------END ARM---------------
+//-------Motors----------------
+void forward(){
+
+}
+void left(){
+
+}
+void right(){
+
+}
+void Tleft(){
+
+}
+void Tright(){
+  
+}
+//-------End Motors------------
+//------end easy actions-------
+
+
+void setup() { 
+  prizm.PrizmBegin();         // initialize the PRIZM controller
+  prizm.setServoSpeed(2,50);
+  //prizm.setServoPosition(2,0);
+  // prizm.setMotorInvert(2,1);      // invert the direction of DC Motor 1
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  
 }
+
